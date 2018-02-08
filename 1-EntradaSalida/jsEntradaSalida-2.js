8ab4ef8a2b776649688e723c0e1fbd6c4ef42c50
jorgenico <36174361+jorgenico@users.noreply.github.com>
2018-02-08 09:06:33 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -15,10 +15,9 @@ es un bloque
 */
   //  var person = prompt("Please enter your name", "Harry Potter");
 
-nombre=prompt("ingrese su nombre","natalia natalia"); 
-alert(nombre);
+nombre=prompt("ingrese su nombre"); 
 
-alert("sunombre es:"+ nombre);
+alert(" su nombre es: " + nombre);
 
 }
 
