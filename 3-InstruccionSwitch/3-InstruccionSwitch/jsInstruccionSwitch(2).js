ef72d02bb394df0841de97d6ad5772a1caf35ed7
jorgenico <36174361+jorgenico@users.noreply.github.com>
2018-02-19 19:40:42 -0300

    Add files via upload

---
+++ 3-InstruccionSwitch/3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -0,0 +1,90 @@
+function Mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+
+alert (mesDelAño);
+
+switch(mesDelAño)
+{
+	case "Enero" :
+		{
+		alert("Ya pasamos el frio, ahora calor!!!.");
+		break;
+		}
+
+	case "Febrero" :
+		{
+		alert("Ya pasamos el frio, ahora calor!!!.");
+		break;
+		}
+
+	case  "Marzo" :
+		{
+		alert("falta para el invierno");
+		break;	
+		}
+
+	case "Abril" :
+		{
+		alert("falta para el invierno");
+		break;	
+		}
+	
+	case "Mayo" :
+		{
+		alert("falta para el invierno");
+		break;
+		}
+
+	case "Junio" :
+		{
+		alert("falta para el invierno");
+		break;
+		}
+	
+	case "Julio" :
+		{
+		alert("Abrigate que hace frio.");
+		break;
+		}
+	
+	case "Agosto" :
+		{
+		alert("Abrigate que hace frio.");
+		break;
+		}
+	
+	case "Septiembre" :
+		{
+		alert("Ya pasamos el frio, ahora calor!!!.");
+		break;
+		}
+
+	case "Octubre" :	
+		{	
+		alert("Ya pasamos el frio, ahora calor!!!.");
+		break;
+		}
+
+	case "Noviembre" :
+		{
+		alert("Ya pasamos el frio, ahora calor!!!.");
+		break;
+		}
+
+	case "Diciembre" :
+		{
+		alert("Ya pasamos el frio, ahora calor!!!.");
+		break;
+		}
+
+}
+
+
+// NO PUSE LOS MESES DE ESTA FORMA "ENERO", "FEBRERO"
+
+
+
+
+}//FIN DE LA FUNCIÓN
