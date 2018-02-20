4be48e7a9a2b8aa2650d02fc096f6a017a549b98
jorgenico <36174361+jorgenico@users.noreply.github.com>
2018-02-19 21:33:12 -0300

    jorge nicolas maroulis

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,12 +1,48 @@
 function Mostrar()
+//Al ingresar una hora, informar: 
+/*Al ingresar una hora, informar: 
+si está entre las 7 y las 11 : "Es de mañana.".*/
+
 {
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
 
-//alert (laHora);
+switch(laHora)
+	{
 	
+		case "7"  :
+		{
+		alert("Es de mañana.");
+		break;
+		}
 	
+		case "8"  :
+		{
+		alert("Es de mañana.");
+		break;
+		}
+	
+		case "9"  :
+		{
+		alert("Es de mañana.");
+		break;
+		}
+	
+		case "10"  :
+		{
+		alert("Es de mañana.");
+		break;
+		}
+	
+		case "11"  :
+		{
+		alert("Es de mañana.");
+		break;
+		}
 
-
+	}
 
 }//FIN DE LA FUNCIÓN
+
+// NO PUSE "" ENTRE LOS PUNTOS ("7" CORRECTO)
+// PUSE LOS NUMEROS DE LAS 24 HS INNECESARIAMENTE YA QUE ME PIDE INFORMACION RESPECTO DE DOS NUMEROS
